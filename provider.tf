8228617c670b45853f0acae5f91d7e68193d6a0b
0xF7d <103605826+0xF7d@users.noreply.github.com>
2022-04-13 17:37:04 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,7 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = "/~.aws/credentials"
+  #shared_credentials_files = "~/.aws/credentials"
+  #breaks terraform plan ^^
   profile                  = "vscode"
 }
